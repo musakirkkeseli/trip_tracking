c4599dd19384bf8d64bb0fef97867d1bab903608
Musa KIRKKESELİ <musakirkkeseli@gmail.com>
2023-11-01 23:21:59 +0300

    create constants_string

---
+++ lib/features/utlility/constants_string.dart
@@ -0,0 +1,8 @@
+class ConstantsString {
+  ConstantsString._();
+
+  static const String locale = "tr_TR";
+  static const String errorMessage = "Beklenmedik bir hata oluştu";
+  static const String datePickerTag = "datePicker";
+  static const String searchHintText = "İl ismi giriniz";
+}
