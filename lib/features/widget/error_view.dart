c7d0da7b8e999210d040fee2e43ff12584f454a6
Musa KIRKKESELİ <musakirkkeseli@gmail.com>
2023-11-03 23:32:18 +0300

    create error_widget

---
+++ lib/features/widget/error_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:trip_tracking/features/utlility/constants_string.dart';
+
+class ErrorView extends StatelessWidget {
+  const ErrorView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(ConstantsString.errorMessage),
+      ),
+    );
+  }
+}
