aabe6d56ad3be5d514bbfdb61d3eda5f7835ff16
Musa KIRKKESELİ <musakirkkeseli@gmail.com>
2023-11-07 20:43:33 +0300

    add FetchData operation in home view

--- lib/view/home/view/home_view.dart
+++ lib/view/home/view/home_view.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:trip_tracking/features/model/trip_request_model.dart';
+import 'package:trip_tracking/features/model/trip_response_model.dart';
 import 'package:trip_tracking/features/widget/error_view.dart';
 import 'package:trip_tracking/view/home/bloc/home_bloc.dart';
+import 'package:trip_tracking/view/home/view/subview/home_success_widget.dart';
 import 'package:trip_tracking/view/home/view/subview/home_view_subview.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    context.read<HomeBloc>().add(FetchData(TripRequestModel()));
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,11 +29,18 @@ class HomeView extends StatelessWidget {
       builder: (context, state) {
         switch (state.status) {
           case PostStatus.initial:
-            return const HomeWidget();
+            return const HomeWidget(
+              customWidget: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
           case PostStatus.success:
-            return const HomeWidget();
+            return HomeWidget(
+              customWidget: HomeSuccessWidget(
+                  responseModel: state.responseModel ?? TripResponseModel()),
+            );
           default:
-            return const ErrorView();
+            return const HomeWidget(customWidget: ErrorView());
         }
       },
     );
